30b4e35107e13b5724cc24ccdd1410e64075c221
Karen Kirr <kkirr77@yahoo.com>
2015-03-01 13:47:54 -0800

    add custom category template test

---
+++ wp-content/themes/twentyfifteen/single-category-world.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * The template for displaying all single posts and with the category world
+ *
+ * @package WordPress
+ * @subpackage Twenty_Fifteen
+ * @since Twenty Fifteen 1.0
+ */
+
+
+echo "testing world single template" ; ?>
