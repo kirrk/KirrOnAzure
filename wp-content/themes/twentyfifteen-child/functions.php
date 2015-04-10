819583f1b2f0901606fd5979287a5fed03f2cec2
unknown <v-kakirr@8248.redmond.corp.microsoft.com>
2015-04-10 12:07:15 -0700

    update files

--- wp-content/themes/twentyfifteen-child/functions.php
+++ wp-content/themes/twentyfifteen-child/functions.php
@@ -9,4 +9,10 @@ function theme_enqueue_styles() {
     );
 }
 
+add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+function theme_enqueue_styles() {
+    wp_enqueue_style( 'XYZ', get_template_directory_uri() . 'css/style.css' );
+
+}
+
 ?>
