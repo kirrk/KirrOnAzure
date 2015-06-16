afb597ad6071bf95fa1af074650f6a0b70e3b1ee
Karen Kirr <kkirr77@yahoo.com>
2015-06-15 18:56:45 -0700

    update front-page

---
+++ wp-content/themes/help-is-here/front-page.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * The main front page template file.
+ *
+ * @package help-is-here
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+		<?php if ( have_posts() ) : ?>
+
+			<?php /* Start the Loop */ ?>
+			<?php while ( have_posts() ) : the_post(); ?>
+
+				<?php
+
+			<?php endwhile; ?>
+
+			<?php the_posts_navigation(); ?>
+
+		<?php else : ?>
+
+			<?php get_template_part( 'template-parts/content', 'none' ); ?>
+
+		<?php endif; ?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
