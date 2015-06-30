92d15ce5a2e4258e926fd9194d9e7f3f5f79cf99
Karen Kirr <kkirr77@yahoo.com>
2015-06-30 11:01:43 -0700

    update to add dynamic tweets via oauth

---
+++ wp-content/themes/help-is-here/template-parts/content-tweets.php
@@ -0,0 +1,22 @@
+<?php
+session_start();
+require "twitteroauth/autoload.php";
+
+$twitteruser = "kkirr2";
+$notweets = 3;
+$consumerkey = "YWFZSU5Q6rCbTjjDkY1R7NW3A";
+$consumersecret = "NNKAGTfQMEe7L1Z7YOaKHPa8moMtoPXZwkfsfpaSEK3E9HSXl5";
+$accesstoken = "337568622-GXvhtCEnq6D0FImnwXmaiUtKMDEj2UKMOF0pCuLO";
+$accesstokensecret = "0IANdstX6sbj01BNViKKjukBAM37Ilz0vJa7WHC4fPvrV";
+
+function getConnectionWithAccessToken($cons_key, $cons_secret, $oauth_token, $oauth_token_secret) {
+  $connection = new TwitterOAuth($cons_key, $cons_secret, $oauth_token, $oauth_token_secret);
+  return $connection;
+}
+
+$connection = getConnectionWithAccessToken($consumerkey, $consumersecret, $accesstoken, $accesstokensecret);
+
+$tweets = $connection->get("https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=".$twitteruser."&count=".$notweets);
+
+echo json_encode($tweets);
+?>
