4434d8467cc0e442325ad01f13a70be443995d96
Karen Kirr <kkirr77@yahoo.com>
2015-06-30 02:41:03 -0700

    update styles

--- wp-content/themes/help-is-here/header.php
+++ wp-content/themes/help-is-here/header.php
@@ -24,20 +24,20 @@
 
 	<header id="masthead" class="site-header" role="banner">
 		<div class="site-branding">
-
 			<div class="contain-jumbo">
 				<div class="jumbotron current"><img class="help1" src=<?php echo get_template_directory_uri() . "/img/help1.jpg"; ?> /></div>
 				<div class="jumbotron"><img class="help2" src=<?php echo get_template_directory_uri() . "/img/help2.jpg"; ?> /></div>
 				<div class="jumbotron"><img class="help3" src=<?php echo get_template_directory_uri() . "/img/help3.jpg"; ?>  /></div>
 				<div class="jumbotron"><img class="help4" src=<?php echo get_template_directory_uri() . "/img/help4.jpg"; ?> /></div>
 		</div>
-
 	</div>
-			<br class="clear" />
+
+			<div class="title-wrap">
 			<div class="main-titles">
 			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home" class="siteTitle"><?php bloginfo( 'name' ); ?></a></h1>
 			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
 		</div>
+  </div>
 
 		<nav id="site-navigation" class="main-navigation" role="navigation">
 			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'help-is-here' ); ?></button>
