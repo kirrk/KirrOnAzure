fdb9130507aec14bc8464b2f0951097819ec7127
Karen Kirr <kkirr77@yahoo.com>
2015-07-07 21:04:13 -0700

    update files

--- wp-content/themes/help-is-here/template-parts/content-tweets.php
+++ wp-content/themes/help-is-here/template-parts/content-tweets.php
@@ -6,20 +6,19 @@ $access_token = "337568622-GXvhtCEnq6D0FImnwXmaiUtKMDEj2UKMOF0pCuLO";
 $access_token_secret = "0IANdstX6sbj01BNViKKjukBAM37Ilz0vJa7WHC4fPvrV";
 $nb_of_tweets = 3;
 $include_rts = false;
+$twitteruser = kkirr2
 
  $connection = new TwitterOAuth($consumer_key,$consumer_secret,$access_token,$access_token_secret);
 
-    $tweets = $connection->get('statuses/user_timeline', array('screen_name' => $kkirr2, 'count' => $nb_of_tweets, 'include_rts' => $include_rts));
+    //$tweets = $connection->get('statuses/user_timeline', array('screen_name' => $kkirr2, 'count' => $nb_of_tweets, 'include_rts' => $include_rts));
+    $tweets = $connection->get("https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=".$twitteruser."&count=".$nb_of_tweets);
+
             ?>
     <ul>
 
-        foreach ($tweets as $key => $tweet): ?>
-            <li><?php
-
-                //links
-
-                echo $tweet->text;
-                echo $tweets->text;
+        <li>
+            <?php
+                echo $tweets;
 
             ?></li>
         <?php endforeach ?>
