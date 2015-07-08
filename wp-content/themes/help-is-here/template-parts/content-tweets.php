336a311ea939db1e3575f4997acbf690140995d7
Karen Kirr <kkirr77@yahoo.com>
2015-07-07 19:10:23 -0700

    update files

--- wp-content/themes/help-is-here/template-parts/content-tweets.php
+++ wp-content/themes/help-is-here/template-parts/content-tweets.php
@@ -9,8 +9,8 @@ $settings = array(
     'consumer_key' => "YWFZSU5Q6rCbTjjDkY1R7NW3A",
     'consumer_secret' => "NNKAGTfQMEe7L1Z7YOaKHPa8moMtoPXZwkfsfpaSEK3E9HSXl5"
 
-$url = 'https://api.twitter.com/1.1/statuses/kkirr2.json';
-$getfield = '?username=kkirr2&count=3';
+$url = 'https://api.twitter.com/1.1/statuses/user_timeline.json';
+$getfield = '?count=3&trim_user=true&user_id=kkirr2&screen_name=kkirr2';
 $requestMethod = 'GET';
 $twitter = new TwitterAPIExchange($settings);
 echo $twitter->setGetfield($getfield)
