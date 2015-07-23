0ad0921edbd35552bdc7843e1fc74f71ab35e702
Karen Kirr <kkirr77@yahoo.com>
2015-07-22 18:28:42 -0700

    update formatting

--- wp-content/themes/help-is-here/template-parts/content-tweets.php
+++ wp-content/themes/help-is-here/template-parts/content-tweets.php
@@ -13,7 +13,7 @@ $twitteruser = "ConnectUpKC";
 
 //date format
      function timeSince($time) {
-        $string = date ('y', mktime(0, 0, 0, date("m")  , date("d"), date("Y")));;
+        $string = date ('m', 'd', 'y', mktime(0, 0, 0, date("m")  , date("d"), date("Y")));;
         // $since = time() - strtotime($time);
 
         // $string     = '';
@@ -38,7 +38,8 @@ $twitteruser = "ConnectUpKC";
 
         // $string = ($count == 1) ? '1 ' . $name . ' ago' : $count . ' ' . $name . 's ago';
 
-        echo $string + " ";
+        echo $string;
+        echo '\n'
 
     }
 
