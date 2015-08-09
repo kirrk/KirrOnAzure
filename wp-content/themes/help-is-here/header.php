b99ce0aff507424b18485fda93e0c68f95bbc059
Karen Kirr <v-kakirr@8248.redmond.corp.microsoft.com>
2015-08-09 13:07:48 -0700

    update dimensions

--- wp-content/themes/help-is-here/header.php
+++ wp-content/themes/help-is-here/header.php
@@ -19,7 +19,7 @@
 </head>
 
 <body <?php body_class(); ?>>
-<div id="page" class="hfeed site container">
+<div id="page" class="hfeed site">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'help-is-here' ); ?></a>
 
 	<header id="masthead" class="site-header" role="banner">
