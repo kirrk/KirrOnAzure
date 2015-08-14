747a75c19bba3eab497893413cf5f733aa5f58a6
Karen Kirr <v-kakirr@8248.redmond.corp.microsoft.com>
2015-08-14 14:50:48 -0700

    remove excess

--- wp-content/themes/help-is-here/footer.php
+++ wp-content/themes/help-is-here/footer.php
@@ -14,11 +14,10 @@
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info">
 			<a href="<?php echo esc_url( __( 'http://#.org/', 'Help is Here' ) ); ?>">Help is Here &copy;<?php echo date('Y'); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'help-is-here' ), 'help-is-here', '<a href="http://#.org" rel="designer">help is here</a>' ); ?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
+
 
 <?php wp_footer(); ?>
 
